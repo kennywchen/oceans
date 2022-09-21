536c2822ac0cf4482169c746b20173c0d9389059
kennywchen <kennywchen123@gmail.com>
2022-09-20 23:07:05 -0700

    completed 10 potatoes

--- script.js
+++ script.js
@@ -3,3 +3,8 @@ function change(id, id2){
     document.getElementById(id2).style.display="block";
 }
 
+function crazy(){
+    document.getElementById("thirdoption").style.display="none";
+    alert("Your potato is: raw potato!")
+    document.getElementById("homebutton").style.display="block"
+}
